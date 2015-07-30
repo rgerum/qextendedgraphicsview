1d8b5d8fa7f9964ad6be00b758da950d831ab1dd
Randrian <none@none>
2015-07-30 15:10:33 +0200

    * adapted to PyQt5

--- QExtendedGraphicsView.py
+++ QExtendedGraphicsView.py
@@ -1,15 +1,15 @@
 import sys
-from PyQt4 import QtGui, QtCore
+from PyQt5 import QtGui, QtCore, QtWidgets
 import numpy as np
 
 def PosToArray(pos):
     return np.array([pos.x(), pos.y()])
 
-class QExtendedGraphicsView(QtGui.QGraphicsView):
+class QExtendedGraphicsView(QtWidgets.QGraphicsView):
     def __init__(self):
-        QtGui.QGraphicsView.__init__(self)
+        QtWidgets.QGraphicsView.__init__(self)
 
-        self.scene = QtGui.QGraphicsScene(self)
+        self.scene = QtWidgets.QGraphicsScene(self)
         self.scene_pan = np.array([250,250])
         self.scene_panning = False
         self.last_pos = [0, 0]
@@ -18,17 +18,17 @@ class QExtendedGraphicsView(QtGui.QGraphicsView):
         self.setScene(self.scene)
         self.scene.setBackgroundBrush(QtCore.Qt.black)
 
-        self.scaler = QtGui.QGraphicsPathItem()
+        self.scaler = QtWidgets.QGraphicsPathItem()
         self.scene.addItem(self.scaler)
-        self.origin = QtGui.QGraphicsPixmapItem(QtGui.QPixmap(), self.scaler)
+        self.origin = QtWidgets.QGraphicsPixmapItem(QtGui.QPixmap(), self.scaler)
 
-        self.hud = QtGui.QGraphicsPathItem()
+        self.hud = QtWidgets.QGraphicsPathItem()
         self.scene.addItem(self.hud)
 
         self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
         self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
 
-        self.setMatrix(QtGui.QMatrix(1,0,0,1,0,0))
+        self.setTransform(QtGui.QTransform())
         self.initialized = False
 
     def paintEvent(self, QPaintEvent):
@@ -43,29 +43,29 @@ class QExtendedGraphicsView(QtGui.QGraphicsView):
 
     def fitInView(self):
         rect = self.origin.childrenBoundingRect()
-        self.origin.setMatrix(QtGui.QMatrix(1,0,0,1,0,0))
+        self.origin.setTransform(QtGui.QTransform())#self.origin.setMatrix(QtGui.QMatrix(1,0,0,1,0,0))
         scaleX = self.size().width()/rect.width()
         scaleY = self.size().height()/rect.height()
         scale = min((scaleX, scaleY))
-        self.scaler.setMatrix(QtGui.QMatrix(scale, 0, 0, scale, 0, 0))
+        self.scaler.setTransform(QtGui.QTransform(scale, 0, 0, scale, 0, 0))#self.scaler.setMatrix(QtGui.QMatrix(scale, 0, 0, scale, 0, 0))
         xoff = self.size().width()-rect.width()*scale
         yoff = self.size().height()-rect.height()*scale
-        self.origin.translate( xoff*0.5/scale,  yoff*0.5/scale)
+        self.origin.setTransform(QtGui.QTransform(1, 0, 0, 1, xoff*0.5/scale,  yoff*0.5/scale))#self.setTransform(QtGui.QTransform())#self.origin.translate( xoff*0.5/scale,  yoff*0.5/scale)
         self.panEvent(xoff, yoff)
         self.zoomEvent(scale, QtCore.QPoint(0,0))
 
     def translateOrigin(self, x, y):
-        self.origin.translate(x, y)
+        self.origin.setTransform(QtGui.QTransform(1, 0, 0, 1, x, y))
         self.panEvent(x, y)
 
     def scaleOrigin(self, scale, pos):
         pos = self.mapToScene(pos)
         x, y = (pos.x(), pos.y())
         s0 = self.scaler.transform().m11()
-        self.origin.translate(-x/s0, -y/s0)
-        self.scaler.setMatrix(QtGui.QMatrix(scale, 0, 0, scale, 0, 0), combine=True)
+        self.origin.setTransform(QtGui.QTransform(1, 0, 0, 1, -x/s0, -y/s0), combine=True)
+        self.scaler.setTransform(QtGui.QTransform(scale, 0, 0, scale, 0, 0), combine=True)
         s0 = self.scaler.transform().m11()
-        self.origin.translate(+x/s0, +y/s0)
+        self.origin.setTransform(QtGui.QTransform(1, 0, 0, 1, +x/s0, +y/s0), combine=True)
         self.zoomEvent(scale, pos)
 
     def getOriginScale(self):
@@ -91,7 +91,8 @@ class QExtendedGraphicsView(QtGui.QGraphicsView):
         if self.scene_panning:
             new_pos = PosToArray(self.mapToScene(event.pos()))
             delta = new_pos-self.last_pos
-            self.origin.translate(*delta/self.scaler.transform().m11())
+            self.origin.setTransform(QtGui.QTransform(1, 0, 0, 1, *delta/self.scaler.transform().m11()), combine=True)
+            #self.origin.translate(*delta/self.scaler.transform().m11())
             self.last_pos = new_pos
         super(QExtendedGraphicsView, self).mouseMoveEvent(event)
 
@@ -101,7 +102,7 @@ class QExtendedGraphicsView(QtGui.QGraphicsView):
         super(QExtendedGraphicsView, self).mouseReleaseEvent(event)
 
     def wheelEvent(self, event):
-        if event.delta() > 0:
+        if event.angleDelta().y() > 0:
             self.scaleOrigin(1.1, event.pos())
         else:
             self.scaleOrigin(0.9, event.pos())
